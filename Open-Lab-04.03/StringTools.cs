c09d0790dd7ee3a2de3b2f6dcf31fa740037b846
DarkVoidSvk <samueljakubecsk@gmail.com>
2021-09-29 09:08:52 +0200

    Tried to remove first and last letter from string.

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+using System.Runtime;
+using System.Linq;
 namespace Open_Lab_04._03
 {
     public class StringTools
     {
-        public string RemoveFirstLast(string original)
+        public string RemoveFirstLast(string original_String)
         {
-            throw new NotImplementedException();
+            string v = original_String.Remove(0, 1);
+            string Removed_First = v;
+            string a = Removed_First.Remove(Removed_First.Length - 1, 1);
+            string Removed_Last = a;
+            return Removed_Last;
         }
     }
 }
